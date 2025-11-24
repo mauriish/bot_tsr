1dd00ac2d894796b79ba6a172c240c4c19cc6097
Mauricio <mausilis@icloud.com>
2025-11-24 03:35:52 -0600

    T_fixedAdd

--- commands/addPoints.js
+++ commands/addPoints.js
@@ -4,14 +4,14 @@ module.exports = {
 
 $setUserVar[points;$sum[$getUserVar[points;$mentioned[1];;racerstats];$message[2]];$mentioned[1];;racerstats]
 
-$if[$sum[$getUserVar[points;$mentioned[1];;racerstats];$message[2]] => 100;
-$giveRole[$guildID;$mentioned[1];1442350209958023198] 
-$setUserVar[license;Oro;$mentioned[1];;racerstats]
-$description[1;Ascendiste a <@&1442350209958023198>!! 
-✅ Agregaste **$message[2]** puntos a **<@$mentioned[1]>**, ahora tiene **$sum[$getUserVar[points;$mentioned[1];;racerstats];$message[2]]** puntos]
-;
-$description[1;✅ Agregaste **$message[2]** puntos a **<@$mentioned[1]>**, ahora tiene **$getUserVar[points;$mentioned[1];;racerstats]** puntos]
+$if[$getUserVar[points;$mentioned[1];;racerstats] >= 100;
+  $giveRole[$guildID;$mentioned[1];1442350209958023198] 
+  $setUserVar[license;Oro;$mentioned[1];;racerstats]
+  $description[1;Ascendiste a <@&1442350209958023198>!! 
+  ✅ Agregaste **$message[2]** puntos a **<@$mentioned[1]>**, ahora tiene **$getUserVar[points;$mentioned[1];;racerstats]** puntos];
+  $description[1;✅ Agregaste **$message[2]** puntos a **<@$mentioned[1]>**, ahora tiene **$getUserVar[points;$mentioned[1];;racerstats]** puntos]
 ]
+
 
 `
 }
