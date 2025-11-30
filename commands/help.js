06f872ee0a34cd6659093e52f0eb6943df0f2c16
Mauricio Silis <mausilis@icloud.com>
2025-11-29 18:58:03 -0600

    help command added and removed setminimumpoints

---
+++ commands/help.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "help",
+    aliases: ["comandos", "ayuda"],
+    description: "Muestra todos los comandos disponibles",
+    async execute(message, args) {
+        const embed = new EmbedBuilder()
+            .setTitle("**Comandos del Bot TSR**")
+            .setDescription("**Lista de todos los comandos disponibles:**")
+            .setColor(0x346beb)
+            .addFields(
+                {
+                    name: "**📊 Comandos de Puntos**",
+                    value: 
+                        "**!add @usuario <cantidad>** - Agrega puntos a un usuario\n" +
+                        "**!remove @usuario <cantidad>** - Quita puntos a un usuario\n" +
+                        "**!points @usuario (@ opcional) ** - Muestra los puntos de un usuario\n",
+                    inline: false
+                },
+            )
+            .setThumbnail(message.client.user.displayAvatarURL())
+            .setFooter({ text: `Solicitado por ${message.author.username}` })
+            .setTimestamp();
+
+        await message.channel.send({ embeds: [embed] });
+    }
+};
